5020e3f24043c736037de8639de5731673a816ef
Stuart Smith <ssmith_1975@yahoo.com>
2017-07-16 14:36:58 -0700

    inital commit of ICC On windows 10

---
+++ HansenIOTWindows10/HansenIOTWindows10/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HansenIOTWindows10.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace HansenIOTWindows10.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
